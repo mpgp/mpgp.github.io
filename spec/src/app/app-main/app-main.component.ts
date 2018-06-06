16b336192952437563411ed7f2114bef3912dc02
loktionov129 <loktionov129@gmail.com>
2018-06-06 22:44:17 +0700

    Add button for scroll to top

--- spec/src/app/app-main/app-main.component.ts
+++ spec/src/app/app-main/app-main.component.ts
@@ -1,5 +1,5 @@
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, ViewEncapsulation } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -11,6 +11,26 @@ import { Observable } from 'rxjs';
 })
 export class AppMainComponent {
   isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
+  navIsFixed: boolean;
 
   constructor(private readonly breakpointObserver: BreakpointObserver) {}
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+      this.navIsFixed = true;
+    } else if ((this.navIsFixed && window.pageYOffset) || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+      this.navIsFixed = false;
+    }
+  }
+
+  scrollToTop(): void {
+    (function smoothscroll(): void {
+      const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > 0) {
+        window.requestAnimationFrame(smoothscroll);
+        window.scrollTo(0, currentScroll - currentScroll / 5);
+      }
+    })();
+  }
 }
