f9a62890da00a02f957bc58a6a298f9b63a38fcf
loktionov129 <loktionov129@gmail.com>
2018-06-16 15:16:24 +0700

    fix(docs): add subject componentDestroyed$

--- src/app/features/docs/containers/docs-root/docs-root.component.ts
+++ src/app/features/docs/containers/docs-root/docs-root.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { filter, map } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { filter, map, takeUntil } from 'rxjs/operators';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -9,18 +10,23 @@ import { filter, map } from 'rxjs/operators';
   styleUrls: ['./docs-root.component.scss'],
   templateUrl: './docs-root.component.html',
 })
-export class DocsRootComponent implements OnInit {
+export class DocsRootComponent implements OnInit, OnDestroy {
+  private readonly componentDestroyed$ = new Subject<boolean>();
+
   constructor(private readonly route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.params
       .pipe(
+        takeUntil(this.componentDestroyed$),
         map(({ id }) => document.getElementById(id)),
         filter(element => !!element),
       )
-      // FIXME: unsub.
-      .subscribe(element => {
-        element.scrollIntoView();
-      });
+      .subscribe(element => element.scrollIntoView());
+  }
+
+  ngOnDestroy(): void {
+    this.componentDestroyed$.next(true);
+    this.componentDestroyed$.unsubscribe();
   }
 }
