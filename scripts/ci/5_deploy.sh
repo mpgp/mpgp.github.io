9e4fab7ceea08cc07c6014c3b6088edec2fc4612
Alexander Loktionov <loktionov129@gmail.com>
2020-10-24 21:22:50 +0300

    fix: test

--- scripts/ci/5_deploy.sh
+++ scripts/ci/5_deploy.sh
@@ -1,8 +1,5 @@
 COMMANDS_TO_RUN+=('echo 5_deploy.sh ...')
 
-COMMANDS_TO_RUN+=('echo Generate coverage info and deploy it to coveralls.io')
-COMMANDS_TO_RUN+=('cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js')
-
 COMMANDS_TO_RUN+=('echo Copy ci files for prevent errors when deploying')
 COMMANDS_TO_RUN+=('cp .travis.yml ./dist')
 COMMANDS_TO_RUN+=('cp -r .circleci ./dist')
